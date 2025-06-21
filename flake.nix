70d04d037b1608ca6c28a728103541a18d86dc6b
Daniel Bonofiglio <dev@dan.uy>
2025-06-20 21:05:08 -0300

    chore: create nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Keep two Bitwarden/Vaultwarden instances in sync";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "bitwarden-portal";
+          version = "1.0.0";
+
+          src = ./.;
+
+          buildInputs = with pkgs; [
+            python314
+            curlMinimal
+            bash
+            openssl
+            ncurses
+            coreutils
+            gnumake
+            libgcc
+            gnugrep
+            binutils
+            findutils
+            wget
+            unzip
+            gnutar
+            jq
+            bitwarden-cli
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp bitwarden-portal.sh $out/bin/
+            chmod +x $out/bin/bitwarden-portal
+          '';
+        };
+      });
+}
