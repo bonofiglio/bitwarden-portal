e6b35905b7c86127e1175c656169169ec5d632ce
Daniel Bonofiglio <dev@dan.uy>
2025-06-20 23:02:29 -0300

    fix: streamline dependencies. wrap program with required runtime dependencies in path

--- flake.nix
+++ flake.nix
@@ -12,41 +12,29 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        bitwarden-portal = pkgs.stdenv.mkDerivation {
-          pname = "bitwarden-portal";
-          version = "1.0.0";
-
-          src = ./.;
-
-          runtimeInputs = with pkgs; [
-            python314
-            curlMinimal
-            bash
-            openssl
-            ncurses
-            coreutils
-            gnumake
-            libgcc
-            gnugrep
-            binutils
-            findutils
-            wget
-            unzip
-            gnutar
-            jq
-            bitwarden-cli
-          ];
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp bitwarden-portal.sh $out/bin/bitwarden-portal
-            chmod +x $out/bin/bitwarden-portal
-          '';
-        };
       in {
-        packages.default = bitwarden-portal;
         overlays.default = final: prev: {
-            bitwarden-portal = bitwarden-portal;
+            bitwarden-portal = pkgs.stdenv.mkDerivation {
+              pname = "bitwarden-portal";
+              version = "1.0.0";
+
+              src = ./.;
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp bitwarden-portal.sh $out/bin/bitwarden-portal
+                chmod +x $out/bin/bitwarden-portal
+
+                wrapProgram $out/bin/bitwarden-portal \
+                  --prefix PATH : ${final.lib.makeBinPath [
+                    pkgs.openssl
+                    pkgs.coreutils
+                    pkgs.findutils
+                    pkgs.jq
+                    pkgs.bitwarden-cli
+                  ]}
+              '';
+            };
         };
       });
 }
